7355cf43da21a50847e93f965542bcae36d198ca
Mikle Green <mikle-green@users.noreply.github.com>
2022-02-01 18:47:05 +0300

    Update tests

--- test/test_user.py
+++ test/test_user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 import logging
-import aiorgwadmin
 import unittest
 import uuid
 
+import aiorgwadmin
 from aiorgwadmin.user import RGWUser
 from . import get_environment_creds
 
@@ -13,22 +13,28 @@ logging.basicConfig(level=logging.WARNING)
 
 class RGWUserTest(unittest.IsolatedAsyncioTestCase):
 
-    def setUp(self):
-        rgw = aiorgwadmin.RGWAdmin(**get_environment_creds())
-        aiorgwadmin.RGWAdmin.set_connection(rgw)
+    async def asyncSetUp(self):
+        self.rgw = aiorgwadmin.RGWAdmin(**get_environment_creds())
+        aiorgwadmin.RGWAdmin.set_connection(self.rgw)
+
+        self.user = f"user-{uuid.uuid4()}"
+
+    async def asyncTearDown(self):
+        users = await self.rgw.get_users()
+
+        if self.user in users:
+            await self.rgw.remove_user(uid=self.user, purge_data=True)
 
     async def test_create_user(self):
-        user_id = f"bucket-{uuid.uuid4()}"
-        display_name = f"Test Create Bucket"
-        u = await RGWUser.create(user_id=user_id, display_name=display_name)
-        self.assertTrue(u.user_id == user_id and
+        display_name = f"Test Create User"
+        u = await RGWUser.create(user_id=self.user, display_name=display_name)
+        self.assertTrue(u.user_id == self.user and
                         u.display_name == display_name)
         await u.delete()
 
     async def test_user_exists(self):
-        user_id = f"bucket-{uuid.uuid4()}"
         display_name = "Test User Exists"
-        u = await RGWUser.create(user_id=user_id, display_name=display_name)
+        u = await RGWUser.create(user_id=self.user, display_name=display_name)
         self.assertTrue(await u.exists())
         await u.delete()
         self.assertFalse(await u.exists())
@@ -36,9 +42,8 @@ class RGWUserTest(unittest.IsolatedAsyncioTestCase):
         self.assertTrue(await u.exists())
 
     async def test_set_quota(self):
-        user_id = f"bucket-{uuid.uuid4()}"
         display_name = "Test Set Quota"
-        u = await RGWUser.create(user_id=user_id, display_name=display_name)
+        u = await RGWUser.create(user_id=self.user, display_name=display_name)
         u.user_quota.size = 1024000
         await u.save()
         nu = await RGWUser.fetch(u.user_id)
